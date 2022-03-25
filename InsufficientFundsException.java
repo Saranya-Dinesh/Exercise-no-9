96b22fb840bf0788f88cf76a9d57e0b9e0fa4b88
Saranya-Dinesh <102136479+Saranya-Dinesh@users.noreply.github.com>
2022-03-25 09:13:46 +0530

    Update InsufficientFundsException.java

--- InsufficientFundsException.java
+++ InsufficientFundsException.java
@@ -1,11 +1,5 @@
 package org.kiot.insufficientfundsexception;
 
-/**
- *	InsufficientFundsException class handle the exception
- *	@version 04/03/2022
- *  @author praveen kandhan
- */
-
 public class InsufficientFundsException extends Exception{
 	String errorMessage;
 
